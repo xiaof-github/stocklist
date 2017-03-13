d8501295b99ee45d5a69a95eb383faa9ca106c9c
xiaof <xffit@163.com>
2017-03-13 22:41:19 +0800

    test rebase 3

--- src/main/webapp/src/js/controllers/listController.js
+++ src/main/webapp/src/js/controllers/listController.js
@@ -7,6 +7,7 @@ var listController = function ($scope, $http) {
     $scope.fetchStockList = function () {
         $http.get('api/list', {code: "000024"} ).success(function(stockList){
              $scope.stock = stockList;
+             console.log(stocklist);
         })
 
     }
