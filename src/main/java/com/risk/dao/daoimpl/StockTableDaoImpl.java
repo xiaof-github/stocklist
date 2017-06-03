a1a32b654aa058e78d2995c26b7ff211fc49d0e0
xiaof <xffit@163.com>
2017-06-03 22:43:08 +0800

    1.实现启动时创建表功能

---
+++ src/main/java/com/risk/dao/daoimpl/StockTableDaoImpl.java
@@ -0,0 +1,38 @@
+package com.risk.dao.daoimpl;
+
+import com.risk.dao.StockTableDao;
+import com.risk.entity.KavgEntity;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by xiaof on 2017/6/3.
+ */
+@Repository
+public class StockTableDaoImpl implements StockTableDao {
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    public int createNewTable(@Param(value = "tableName") String tableName){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        int ret = sqlSession.update("com.risk.dao.StockTableDao.createNewTable", tableName);
+        sqlSession.commit();
+        sqlSession.close();
+        return ret;
+    }
+
+    public int dropTable(@Param(value = "tableName") String tableName){
+        return 0;
+    }
+
+    public int existTable(String tableName){
+        return 0;
+    }
+
+    public int insertKavg(KavgEntity entity){
+        return 0;
+    }
+}
---
+++ src/main/java/com/risk/entity/KavgEntity.java
@@ -0,0 +1,34 @@
+package com.risk.entity;
+
+/**
+ * Created by xiaof on 2017/5/28.
+ */
+public class KavgEntity {
+    private Long id;
+    private String name;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
